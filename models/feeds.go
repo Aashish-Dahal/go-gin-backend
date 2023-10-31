2dfe9235e9b461fb41f9cd12ac10c1a722e50e9f
Aashish-Dahal <ashishdahal490@gmail.com>
2023-10-31 15:13:02 +0545

    fix: feed models and migration file

--- models/feeds.go
+++ models/feeds.go
@@ -1,16 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Feed struct {
-	UserID    int       `json:"user_id" validate:"required"`
-	User      *User     `json:"user,omitempty"`
-	Status    string    `json:"status"`
-	Tags      []string  `json:"tags"`
-	Images    []string  `json:"images"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	UserID    int            `json:"user_id" validate:"required"`
+	User      *User          `json:"user,omitempty"`
+	Status    string         `json:"status"`
+	Tags      string         `json:"tags"`
+	Images    string         `json:"images"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
 
 // TableName gives table name of model
