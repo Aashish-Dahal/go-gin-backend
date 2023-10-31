613394d2ef371a45ae5f044e0343cc4d0fbd6436
Aashish-Dahal <ashishdahal490@gmail.com>
2023-10-31 15:10:59 +0545

    feat: GetFeedByID, GetAllFeeds,DeleteFeed controller

--- api/controllers/feeds.go
+++ api/controllers/feeds.go
@@ -5,6 +5,7 @@ import (
 	"boilerplate-api/errors"
 	"boilerplate-api/infrastructure"
 	"boilerplate-api/models"
+	"boilerplate-api/paginations"
 	"boilerplate-api/responses"
 	"net/http"
 
@@ -42,4 +43,46 @@ func (cc FeedController) CreateFeed(c *gin.Context) {
 	}
 	responses.SuccessJSON(c, http.StatusOK, "Feed Created Successfully")
 
+}
+func (cc FeedController) GetAllFeeds(c *gin.Context) {
+	pagination := paginations.BuildPagination[*paginations.FeedPagination](c)
+
+	feeds, count, err := cc.feedService.GetAllFeeds(*pagination)
+	if err != nil {
+		cc.logger.Zap.Error("Error finding feed records", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to get feeds data")
+		responses.HandleError(c, err)
+		return
+	}
+
+	responses.JSONCount(c, http.StatusOK, feeds, count)
+}
+func (cc FeedController) GetFeedByID(c *gin.Context) {
+	pagination := paginations.BuildPagination[*paginations.FeedPagination](c)
+
+	idParam := c.Param("id")
+
+	feeds, count, err := cc.feedService.GetFeedByID(*pagination,idParam)
+	if err != nil {
+		cc.logger.Zap.Error("Error finding user feeds", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to get users feed data")
+		responses.HandleError(c, err)
+		return
+	}
+
+	responses.JSONCount(c, http.StatusOK, feeds, count)
+}
+func (cc FeedController) DeleteFeed(c *gin.Context) {
+
+	idParam := c.Param("id")
+
+	 err := cc.feedService.DeleteFeed(idParam)
+	if err != nil {
+		cc.logger.Zap.Error("Error finding feeds", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to delete feed data")
+		responses.HandleError(c, err)
+		return
+	}
+
+	responses.SuccessJSON(c, http.StatusOK, "Feed deleted Successfully")
 }
