71353474d19479c976c4ed155ec7313e5a92bab5
Aashish-Dahal <ashishdahal490@gmail.com>
2023-10-31 15:12:07 +0545

    feat: GetFeedByID, GetAllFeeds,DeleteFeed service

--- api/services/feeds.go
+++ api/services/feeds.go
@@ -2,7 +2,9 @@ package services
 
 import (
 	"boilerplate-api/api/repository"
+	"boilerplate-api/dtos"
 	"boilerplate-api/models"
+	"boilerplate-api/paginations"
 )
 
 type FeedService struct {
@@ -19,4 +21,20 @@ func NewFeedService(repository repository.FeedRepository) FeedService {
 func (c FeedService) CreateFeed(feed models.Feed) error {
 	err := c.repository.Create(feed)
 	return err
+}
+
+// GetAllFeeds to get all the Feeds
+func (c FeedService) GetAllFeeds(pagination paginations.FeedPagination) ([]dtos.GetFeedResponse, int64, error) {
+	return c.repository.GetAllFeeds(pagination)
+
+}
+// GetFeedsWithID Get feeds with ID
+func (c FeedService) GetFeedByID(pagination paginations.FeedPagination,userID string) ([]dtos.GetFeedResponse, int64, error) {
+	return c.repository.GetFeedByID(pagination,userID)
+
+}
+// DeleteFeed to delete the feed
+func (c FeedService) DeleteFeed(id string) (error) {
+	return c.repository.DeleteFeed(id)
+
 }
